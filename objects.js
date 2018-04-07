b39bb1074c6767ecaacc6a3ce16f3f31d043c910
Aviram cohen <justaviram@gmail.com>
2018-04-07 20:01:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artist, song ){
   playlist[artist] = song;
   return playlist;
 }
+function removeFromPlayList(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
